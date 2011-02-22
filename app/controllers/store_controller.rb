ee0c8d8952293cf65bb9b5a9b9ae1bbbd22b9283
hj lee <kerdosa@gmail.com>
2011-02-22 00:17:46 -0700

    ajax starting

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @cart = current_cart
     
+    # Show how many times visited the store
     if session[:counter].nil?
       session[:counter] = 1
     else
